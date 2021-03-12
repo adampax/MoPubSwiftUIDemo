3c195efe16c7647700309e38b525eee8457dec09
Adam Paxton <adampaxton@polancomedia.com>
2021-03-12 15:56:44 -0600

    Test a 320x250 Banner

--- MoPubSwiftUIDemo/BannerExampleView.swift
+++ MoPubSwiftUIDemo/BannerExampleView.swift
@@ -15,6 +15,9 @@ struct BannerExampleView: View {
                 .padding()
             MoPubBannerView(adUnitID: "0ac59b0996d947309c33f59d6676399f", adSize: CGSize(width: 320, height: 50))
             
+            Text("320x250 Banner (MRect)")
+                .padding()
+            MoPubBannerView(adUnitID: "2aae44d2ab91424d9850870af33e5af7", adSize: CGSize(width: 320, height: 250))
             
         }.navigationTitle("Banners")
     }
